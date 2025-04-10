f0e4fae5efd6495ce4b413f44893aaada65883b3
Fabio-Ceriaco <ceriacofabio@gmail.com>
2025-04-10 13:40:10 +0100

    Create main layout components

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-main-layout pageTitle="Contries & Capitals Quiz">
+    <h1 class="text-center my-5">HOME PAGE</h1>
+</x-main-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Stmt\Return_;
 
-Route::get('/', function () {
-    echo 'Countries and capitals';
-});
+Route::view('/', 'home');
 
 
 Route::get('/show_data', [MainController::class, 'showData']);
