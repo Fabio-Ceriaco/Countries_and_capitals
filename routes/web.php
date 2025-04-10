caf6ee5929f9938cc883914c18895b2152f4efca
Fabio-Ceriaco <ceriacofabio@gmail.com>
2025-04-10 13:08:49 +0100

    Upload data to project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Return_;
 
 Route::get('/', function () {
     echo 'Countries and capitals';
 });
+
+
+Route::get('/show_data', [MainController::class, 'showData']);
